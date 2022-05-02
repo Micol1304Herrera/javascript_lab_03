61c75f5c5c4c752c9e1f394c4c61565d65e33a03
micol <micol.herrera@soymas.cl>
2022-05-02 12:11:37 -0400

    second commit

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert("hola inmundo")
+var foco = document.getElementById("foco") 
+var botonOff = document.getElementById("off")
+var botonOn = document.getElementById("on")
+ 
+botonOn.addEventListener("click",function(){
+    console.log("on")
+    foco.src = "https://www.w3schools.com/js/pic_bulbon.gif"
+})
+botonOff.addEventListener("click",function(){
+    console.log("off")
+    foco.src = "https://www.w3schools.com/js/pic_bulboff.gif"
+})
